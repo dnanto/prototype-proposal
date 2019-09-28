ca64cda40304bba513107a48fc97c97329a7db8e
dnanto <dnegron2@gmu.edu>
2019-09-28 15:20:37 -0400

    analysis scripts
    for genome and range

--- exp-1/exp.sh
+++ exp-1/exp.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-# full genome analysis
-
 blastdbcmd -db ../data/db -entry all -outfmt "%a %l" | \
 	awk '$2 > 34000 { print $1; }' | \
 	sort -b | \
 	join -t $'\t' - ../data/date.tsv | \
+	awk '$2 != "NA"' | 
 	sort -k 2 | \
 	tee def.tsv | \
 	awk '{ printf "s/^>%s/>%s_%s/\n", $1, $1, $2; }' > def.sed
 
-awk '$2 != "NA" { print $1; }' def.tsv | \
+cut -f 1 def.tsv | \
 	blastdbcmd -db ../data/db -entry_batch - | \
 	sed -f def.sed | \
 	mafft --auto --adjustdirection --thread -1 - > msa.fna
---
+++ exp-2/exp.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+blastdbcmd -db ../data/db -entry FJ643676.1 -range 18233-21073 > qry.fna
+
+blastn -task megablast -query qry.fna -db ../data/db -outfmt "6 saccver" | \
+	blastdbcmd -db ../data/db -entry_batch - > lib.fna
+
+glsearch36 -m 8C -T 8 qry.fna lib.fna > hits.tsv
+
+awk '{ OFS = "\t" } /^[^#]/ { print $2, $9, $10 }' hits.tsv | \
+	sort -b | \
+	join -1 1 -t $'\t' - ../data/date.tsv | \
+	awk '$4 != "NA"' | 
+	sort -k 4 | \
+	tee def.tsv | \
+	awk '{ printf "s/%s:%d-%d/%s:%d-%d_%s/\n", $1, $2, $3, $1, $2, $3, $4 ; }' > def.sed
+
+awk '{ printf "%s %d-%d\n", $1, $2, $3 }' def.tsv | \
+	blastdbcmd -db ../data/db -entry_batch - | \
+	sed -f def.sed | \
+	mafft --auto --adjustdirection --thread -1 - > msa.fna
+
+iqtree -s msa.fna -pre phy -alrt 1000 -bb 1000 -bnni -nt AUTO
